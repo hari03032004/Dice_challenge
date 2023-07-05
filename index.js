c2e7140d8e03783354e78e7c0c2c40b56ee652db
hari03032004 <93466136+hari03032004@users.noreply.github.com>
2023-07-05 23:46:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var random1=prompt("No between 1 to 6");
+var random1=Math.floor( Math.random()*6)+1;;
 var random2=Math.floor( Math.random()*6)+1;
 console.log(random1);
 
@@ -8,12 +8,11 @@ document.querySelectorAll("img")[0].setAttribute("src",randomImage);
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
 
 if(random1 > random2){
-  document.querySelector("h1").textContent = "You win!!!";
+  document.querySelector("h1").textContent = "Player1 win!!!";
 }
 else if( random1 < random2){
-  document.querySelector("h1").textContent = "You lose !!!";
+  document.querySelector("h1").textContent = "Player2 !!!";
 }
 else if (random1 === random2){
   document.querySelector("h1").textContent = "Draw";
 }
-
