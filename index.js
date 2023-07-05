2dff463dd050091942898a898fdf702f7c97aa8d
hari03032004 <93466136+hari03032004@users.noreply.github.com>
2023-07-05 23:24:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var random1=Math.floor( Math.random()*6)+1;
+var random2=Math.floor( Math.random()*6)+1;
+console.log(random1);
+
+var randomImage = "./images/dice"+ random1 + ".png";
+var randomImage2 = "./images/dice"+ random2 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImage);
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+
+if(random1 > random2){
+  document.querySelector("h1").textContent = "Player 1 Wins !!";
+}
+else if( random1 < random2){
+  document.querySelector("h1").textContent = "Player 2 wins !!";
+}
+else{
+  document.querySelector("h1").textContent = "Draw";
+}
